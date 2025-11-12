2f5aab13d8e12c2e4b7d9810fe8f594f6352a529
orenandiniz <orenandiniz@gmail.com>
2025-11-12 18:33:53 +0000

    cadastrando

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,8 +6,19 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado1, estado2;
+  char cidade1[20], cidade2[20], codigo1[20], codigo2[20];
 
   // Área para entrada de dados
+  printf ("----------- Cadastro das cartas ----------\n");
+  printf ("\n");
+  printf ("carta 1:\n");
+  
+  printf ("Digite o estado: ");
+  scanf ("%s", estado1);//Aqui eu coleto o estado que o usuário digitar, pelo que entendi não precisa do & para strings
+
+  printf ("Digite o código da carta:");
+  scanf ("%c", &codigo1);//Coletando o codigo da carta
 
   // Área para exibição dos dados da cidade
 
